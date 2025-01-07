5ccd16524f3968de333604c93717a1fb7d965716
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-01-07 11:00:41 +0530

    Add UI to the game
    I added UI to the game and had to change some Javascript code for it to work. I also added css file to make it presentable.

--- script.js
+++ script.js
@@ -1,5 +1,99 @@
+const play = document.querySelector(".play");
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const result = document.querySelector(".result");
+let humanChoice = "";
+let computerChoice = "";
 let humanScore = 0;
 let computerScore = 0;
+let click = 0;
+
+play.addEventListener("click", () => {
+  play.style.display = "none";
+  rock.style.display = "inline";
+  paper.style.display = "inline";
+  scissors.style.display = "inline";
+});
+
+rock.addEventListener("click", () => {
+  humanChoice = "rock";
+  computerChoice = getComputerChoice();
+  compareAndShowResult();
+  click++;
+  checkRounds();
+});
+
+paper.addEventListener("click", () => {
+  humanChoice = "paper";
+  computerChoice = getComputerChoice();
+  compareAndShowResult();
+  click++;
+  checkRounds();
+});
+
+scissors.addEventListener("click", () => {
+  humanChoice = "scissors";
+  computerChoice = getComputerChoice();
+  compareAndShowResult();
+  click++;
+  checkRounds();
+});
+
+function checkRounds() {
+  if (click === 5) {
+    play.style.display = "inline";
+    rock.style.display = "none";
+    paper.style.display = "none";
+    scissors.style.display = "none";
+    if (humanScore > computerScore) result.textContent = "You won this round!";
+    else if (humanScore < computerScore)
+      result.textContent = "Computer won this round! Better luck next time!";
+    else result.textContent = "It was tie! Play again!";
+  }
+}
+
+function compareAndShowResult() {
+  if (humanChoice === "rock") {
+    if (computerChoice === "paper") {
+      computerScore++;
+      result.textContent = "Computer won!";
+      return;
+    } else if (computerChoice === "scissors") {
+      result.textContent = "You won!";
+      humanScore++;
+      return;
+    } else {
+      result.textContent = "It's a tie!";
+    }
+  }
+  if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      result.textContent = "Computer won!";
+      computerScore++;
+      return;
+    } else if (computerChoice === "rock") {
+      result.textContent = "You won!";
+      humanScore++;
+      return;
+    } else {
+      result.textContent = "It's a tie!";
+    }
+  }
+  if (humanChoice === "scissors") {
+    if (computerChoice === "rock") {
+      result.textContent = "Computer won!";
+      computerScore++;
+      return;
+    } else if (computerChoice === "paper") {
+      result.textContent = "You won!";
+      humanScore++;
+      return;
+    } else {
+      result.textContent = "It's a tie!";
+    }
+  }
+}
 
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
@@ -13,37 +107,3 @@ function getComputerChoice() {
   return choice;
 }
 
-function getHumanChoice() {
-  let choice = prompt("Enter your choice");
-  return choice;
-}
-
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase().trim();
-  if (!["rock", "paper", "scissor"].includes(humanChoice)) {
-    console.log("Enter a valid choice: rock, paper, scissors");
-    return;
-  }
-  if (humanChoice === computerChoice) {
-    console.log("Yay, you won!");
-    humanScore++;
-  } else {
-    console.log("you lost");
-    computerScore++;
-  }
-}
-
-function playGame() {
-  while (humanScore + computerScore !== 5) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-  }
-  console.log(`Your Score: ${humanScore}`);
-  console.log(`Computer Score: ${computerScore}`);
-  if (humanScore > computerScore)
-    console.log("Congratulations! You won this round!");
-  else console.log("You lost, better luck next time!");
-}
-
-playGame();
